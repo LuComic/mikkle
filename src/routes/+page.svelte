a0eef585c905f2fafb06addbf69fa8416c196e7b
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2025-08-31 16:54:34 +0300

    style changes for profiles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -91,9 +91,9 @@
 </script>
 
 <div
-	class="flex h-auto min-h-screen w-screen flex-col items-center justify-center gap-4 bg-[#F6F2E8] p-4 md:pt-20"
+	class="flex h-auto min-h-screen w-screen flex-col items-center justify-center gap-4 bg-[#F6F2E8] p-4 pt-20"
 >
-	<div class="flex items-center justify-center gap-6 md:max-w-[80vw]">
+	<div class="grid grid-cols-4 items-center justify-center gap-4 md:flex md:max-w-[80vw] md:gap-6">
 		<button
 			class={`px-2 py-1 ${activeProfile === 'Default' ? 'border border-r-2 border-b-2 border-black bg-black text-[#F6F2E8]' : 'border hover:border-r-2 hover:border-b-2'}`}
 			onclick={() => selectProfile('Default')}
